e1fbeaaf31c8f904c234c9cdb86010de920279aa
jinwoong <jihoong125@gmail.com>
2026-03-27 14:02:44 +0900

    2751.c

---
+++ silver/2751.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int num[2000001];
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    for(int i=0;i<n;i++){
+        int temp;
+        scanf("%d", &temp);
+        num[temp+1000000]=1;
+    }
+
+    for(int i=0;i<2000001;i++){
+        if(num[i]==1){
+            printf("%d\n", i-1000000);
+        }
+    }
+
+    return 0;
+}
+
